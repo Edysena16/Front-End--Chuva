3495b2eb97ed02bac1765b4b94e9c5881c0293ec
Edysena16 <edvaldofutsena@hotmail.com>
2022-04-30 13:58:02 -0300

    add conteudo resposta, ocultando seu display e dando funcionalidade ao botao resposta para exibir

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function exibir() {
   btnV = document.getElementById('btn-resExp')
   btnV.style.visibility = 'hidden'
 }
+
+function criarTopico() {
+  bt_topic = document.getElementById('criar-topico')
+  bt_topic.style.display = 'contents'
+
+  painel = document.getElementById('discurssao-painel')
+  painel.style.display = 'none'
+}
+
+function exibirResposta() {
+  resp = document.getElementById('content-resp')
+  resp.style.display = 'block'
+}
